ad18e53706df7f3b7a3a38580c8f2d6c48193c91
ashisjena1 <40244393+ashisjena1@users.noreply.github.com>
2018-09-27 15:46:28 +0530

    Add files via upload

---
+++ web_basics/a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Action page</title>
+		<script>
+			Welcome <?php echo $_POST["name"]; ?><br>
+		</script>
+	</head>
+	<body>
+		<h1>Hi Ashis</h1>
+	</body>
+</html>
