d39c835f8e0b4e4491629b7c9a74ba75c5814221
Ashis Kumar Jena <40244393+ashisjena1@users.noreply.github.com>
2018-10-08 10:24:23 +0530

    Add files via upload

--- javaScript/js/script.js
+++ javaScript/js/script.js
@@ -1,7 +1,34 @@
+var fName=document.getElementById("fName");
+var mName=document.getElementById("mName");
+var lName=document.getElementById("lName");
+var dob=document.getElementById("dob");
+var male=document.getElementById("male");
+var female=document.getElementById("female");
+var others=document.getElementById("others");
+var email=document.getElementById("email");
+var phoneNumber=document.getElementById("phoneNumber");
+var pass=document.getElementById("password");
+var cPassword=document.getElementById("cPassword");
+var cAddress=document.getElementById("cAddress");
+var cCity=document.getElementById("cCity");
+var cState=document.getElementById("cState");
+var cCountry=document.getElementById("cCountry");
+var cZipCode=document.getElementById("cZipCode");
+var pAddress=document.getElementById("pAddress");
+var pCity=document.getElementById("pCity");
+var pState=document.getElementById("pState");
+var pCountry=document.getElementById("pCountry");
+var pZipCode=document.getElementById("pZipCode");
+var pInterests=document.getElementsByName("personalInterests");
+var oInterests=document.getElementById("oInterests");
+var others=document.getElementById("others");
+var result=document.getElementById("result");
+var policy=document.getElementById("policy");
+
+
 function validateForm(){
 	
 	var form=document.getElementById("registrationForm");
-
 		
 	if( checkFirstName() && checkMiddleName() && checkLastName() && checkDOB() && checkGender() && checkEmail() && checkPhoneNumber() && checkPassword() && checkConfirmPassword() &&
 		checkCurrentAddress() && checkCurrentCountry() && checkCurrentState() && checkCurrentCity() && checkCurrentZipCode() && checkPermanentAddress() && checkPermanentCountry() &&
@@ -10,20 +37,19 @@ function validateForm(){
 		msg='<h3 style="color:green;text">Congratulations you registered successfully...</h3>'
 		form.innerHTML=msg;
 	}
-	else{
+	/*else{
 		
 		msg='<h2 style="color:red;text">Sorry you entered wrong details or missing something...</h2><br><h4>please fill the details again...</h4>'
 		form.innerHTML=msg;
-	}
+	}*/
 
 	return false;
 }
-function checkFirstName(){
-	fName=document.getElementById("fName");
+function checkFirstName(){	
 	if (fName.value === '') {
 		fName.setCustomValidity('Required first name');
-	} else if (fName.value.match(/[A-Za-z]+/)!=fName.value){
-		fName.setCustomValidity('please enter a valid first name');
+	} else if (fName.value.match(/[A-Za-z]+/)!=fName.value.trim()){
+		fName.setCustomValidity('First Name should contain only alphabets');
 	} else {
 	    fName.setCustomValidity('');
 		return true;
@@ -32,9 +58,8 @@ function checkFirstName(){
 }
 
 function checkMiddleName(){
-	mName=document.getElementById("mName");
-	if (mName.value.match(/[A-Za-z]*/)!=mName.value){
-		mName.setCustomValidity('please enter a valid middle name');
+	if (mName.value.match(/[A-Za-z]*/)!=mName.value.trim()){
+		mName.setCustomValidity('Middle Name should contain only alphabets');
 	} else {
 	   mName.setCustomValidity('');
 	   return true;
@@ -42,11 +67,10 @@ function checkMiddleName(){
 	return false;
 }
 function checkLastName(){
-	lName=document.getElementById("lName");
 	if (lName.value === '') {
 		lName.setCustomValidity('Required last name');
-	} else if (lName.value.match(/[A-Za-z]+/)!=lName.value){
-		lName.setCustomValidity('please enter a valid last name');
+	} else if (lName.value.match(/[A-Za-z]+/)!=lName.value.trim()){
+		lName.setCustomValidity('Last Name should contain only alphabets');
 	} else {
 	    lName.setCustomValidity('');
 		return true;
@@ -54,7 +78,6 @@ function checkLastName(){
 	return false;
 }
 function checkDOB(){
-	dob=document.getElementById("dob");
 	if (dob.value === '') {
 		dob.setCustomValidity('Required Date of Birth');
 	} else if (dob){
@@ -71,19 +94,15 @@ function checkDOB(){
 	return false;
 }
 function checkGender(){
-	
-	var gender=document.getElementsByName("gender");
-	var genderError=document.getElementById("genderError");
-	if(!(gender[0].checked || gender[1].checked || gender[2].checked)){
-		genderError.innerHTML="Please select your gender";
+	if(!(male.checked || female.checked || others.checked)){
+		male.setCustomValidity("Please select your gender");
 		return false;
 	}
-	genderError.innerHTML="";
+	male.setCustomValidity("");
 	return true;
 }
 
 function checkEmail(){
-	var email=document.getElementById("email");
 	if (email.value === '') {
 		email.setCustomValidity('Required email address');
 	} else if (!email.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)){
@@ -97,18 +116,16 @@ function checkEmail(){
 	return false;
 }
 function checkPhoneNumber(){
-	var phoneNumber=document.getElementById("phoneNumber");
 	if (phoneNumber.value === '') {
 		phoneNumber.setCustomValidity('Required phone number');
 	} else if (phoneNumber.value.match(/[1-9][0-9]{9}/)!=phoneNumber.value){
-		phoneNumber.setCustomValidity('please enter a valid phone number');
+		phoneNumber.setCustomValidity('phone number must be 10 digits');
 	} else {
 	    phoneNumber.setCustomValidity('');
 	}
 	return true;
 }
 function checkPassword(){
-	var pass=document.getElementById("password");
 	if (pass.value === '') {
 		pass.setCustomValidity('Required password');
 	} else if (pass.value.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)!=pass.value){
@@ -119,7 +136,6 @@ function checkPassword(){
 	return true;
 }
 function checkConfirmPassword(){
-	var cPassword=document.getElementById("cPassword");
 	if (cPassword.value === '') {
 		cPassword.setCustomValidity('Required confirm password');
 	} else if (cPassword.value.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)==cPassword.value){
@@ -138,7 +154,6 @@ function checkConfirmPassword(){
 	return false;
 }
 function checkCurrentAddress(){
-	var cAddress=document.getElementById("cAddress");
 	if (cAddress.value === '') {
 		cAddress.setCustomValidity('This field should be filled');
 	} else if (cAddress.value.match(/.{15,50}/)!=cAddress.value){
@@ -150,10 +165,9 @@ function checkCurrentAddress(){
 	return false;
 }
 function checkCurrentCity(){
-	var cCity=document.getElementById("cCity");
 	if (cCity.value === '') {
 		cCity.setCustomValidity('Required city');
-	} else if (cCity.value.match(/[A-Za-z]+/)!=cCity.value){
+	} else if (cCity.value.match(/[A-Za-z]+/)!=cCity.value.trim()){
 		cCity.setCustomValidity('please enter a valid city name');
 	} else {
 	    cCity.setCustomValidity('');
@@ -162,7 +176,6 @@ function checkCurrentCity(){
 	return false;
 }
 function checkCurrentState(){
-	var cState=document.getElementById("cState");
 	if (cState.value === '') {
 		cState.setCustomValidity('Required State');
 	} else{
@@ -172,7 +185,6 @@ function checkCurrentState(){
 	return false;
 }
 function checkCurrentCountry(){
-	var cCountry=document.getElementById("cCountry");
 	if (cCountry.value === '') {
 		cCountry.setCustomValidity('Required country');
 	} else{
@@ -183,11 +195,10 @@ function checkCurrentCountry(){
 }
 
 function checkCurrentZipCode(){
-	var cZipCode=document.getElementById("cZipCode");
 	if (cZipCode.value === '') {
 		cZipCode.setCustomValidity('Required zip code');
 	} else if (cZipCode.value.match(/[0-9]{6}/)!=cZipCode.value){
-		cZipCode.setCustomValidity('please enter a valid zip code');
+		cZipCode.setCustomValidity('zip code must be 6 digits');
 	} else {
 	    cZipCode.setCustomValidity('');
 		return true;
@@ -196,7 +207,7 @@ function checkCurrentZipCode(){
 }
 
 function checkPermanentAddress(){
-	var pAddress=document.getElementById("pAddress");
+	
 	if (pAddress.value === '') {
 		pAddress.setCustomValidity('This field should be filled');
 	} else if (pAddress.value.match(/.{15,50}/)!=pAddress.value){
@@ -208,10 +219,10 @@ function checkPermanentAddress(){
 	return false;
 }
 function checkPermanentCity(){
-	var pCity=document.getElementById("pCity");
+	
 	if (pCity.value === '') {
 		pCity.setCustomValidity('Required city');
-	} else if (pCity.value.match(/[A-Za-z]+/)!=pCity.value){
+	} else if (pCity.value.match(/[A-Za-z]+/)!=pCity.value.trim()){
 		pCity.setCustomValidity('please enter a valid city name');
 	} else {
 	    pCity.setCustomValidity('');
@@ -220,7 +231,7 @@ function checkPermanentCity(){
 	return false;
 }
 function checkPermanentState(){
-	var pState=document.getElementById("pState");
+	
 	if (pState.value === '') {
 		pState.setCustomValidity('Required State');
 	} else{
@@ -230,7 +241,7 @@ function checkPermanentState(){
 	return false;
 }
 function checkPermanentCountry(){
-	var pCountry=document.getElementById("pCountry");
+
 	if (pCountry.value === '') {
 		pCountry.setCustomValidity('Required country');
 	} else{
@@ -241,11 +252,11 @@ function checkPermanentCountry(){
 }
 
 function checkPermanentZipCode(){
-	var pZipCode=document.getElementById("pZipCode");
+	
 	if (pZipCode.value === '') {
 		pZipCode.setCustomValidity('Required zip code');
 	} else if (pZipCode.value.match(/[0-9]{6}/)!=pZipCode.value){
-		pZipCode.setCustomValidity('please enter a valid zip code');
+		pZipCode.setCustomValidity('zip code must be 6 digits');
 	} else {
 	    pZipCode.setCustomValidity('');
 		return true;
@@ -253,17 +264,18 @@ function checkPermanentZipCode(){
 	return false;
 }
 function checkPersonalInterests(){
-	var pInterests=document.getElementsByName("personalInterests");
-	if(pInterests[0].checked || pInterests[1].checked || pInterests[2].checked || pInterests[3].checked || pInterests[4].checked || pInterests[5].checked || pInterests[6].checked){
+	if(pInterests[0].checked || pInterests[1].checked || pInterests[2].checked || pInterests[3].checked || pInterests[4].checked || pInterests[5].checked || pInterests[6].checked ||
+		pInterests[7].checked){
 		pInterests[7].setCustomValidity('');
 		return true;
 	}
-	pInterests[7].setCustomValidity("please select any of your personal interests or others");
+	else{
+		pInterests[7].setCustomValidity("please select any of your personal interests or others");
+	}
 	return false;
 }
 function otherInterests(){
-	var oInterests=document.getElementById("oInterests");
-	var others=document.getElementById("others");
+	
 	if(others.checked){
 		oInterests.style.display="block";
 	}else{
@@ -307,20 +319,22 @@ function captcha(){
 }
 
 function checkCaptcha(){
-	result=document.getElementById("result");
+	
 	result.setCustomValidity('');
 	if (captchaResult!=result.value){
 		result.oninvalid="invalid";
 		result.setCustomValidity('try once more...');
 		return false;
 	} 
+	result.setCustomValidity('');
 	return true;
 }
 function checkPolicy(){
-	var policy=document.getElementById('policy');
 	if(!policy.checked){
 		policy.setCustomValidity("Accept the terms and conditions");
 		return false;
+	}else{
+		policy.setCustomValidity("");
+		return true;
 	}
-	return true;
 }
